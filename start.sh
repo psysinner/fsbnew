1c41f0ed9a268ad58387591e7aba499a220d3747
psysinner <psychosinner@proton.me>
2026-02-04 00:37:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+# Activate your virtual environment
+source /root/pluckbots/nezu/.venv/bin/activate
+
+# Install dependencies (fresh, no cache)
 pip install -r requirements.txt --no-cache-dir
+
+# Run your bot
 python3 main.py
